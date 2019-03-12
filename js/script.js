67a4bfb6aab7d1ac4f7f8dcc7539f899a0030be1
Jessica Billingslea <jessicrr@gmail.com>
2019-03-12 15:44:08 -0400

    Added comments

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
+//declaring variables and prompts for user input
+
 var eventType = prompt("Please enter the event type.", "formal, semi-formal or casual");
 
 var tempFahr = prompt("Please enter the temperatue in degrees Farhrenheit.");
 
 var wear = " ";
+
+//conditional statements for clothing suggestion output
 
 if (eventType === "casual" && tempFahr < 54) {
   wear = "a coat and something comfy.";
@@ -26,6 +30,10 @@ if (eventType === "casual" && tempFahr < 54) {
   result = "Invalid input, please refresh the page and try again.";
 }
 
+//declaring result variable to be written to the log
+
 var result = `Since it is ${tempFahr} degrees and you are going to a ${eventType} event, you should wear ${wear}`;
+
+//writing the result to the log
 
 console.log(result);
